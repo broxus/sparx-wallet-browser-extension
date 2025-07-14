6632f409c5bb521111b5d57f1fe00baff70c7835
Aliaksei <milutkin.work@gmail.com>
2025-07-14 14:53:07 +0300

    update ci

--- scripts/update-package-version.sh
+++ scripts/update-package-version.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
+version_type=${1:-patch}
 current_version=$(jq -r '.version' src/manifest/base.json)
 
 increment_patch_version() {
   local version=$1
+  local type=$2
+
   local major=$(echo $version | cut -d. -f1)
   local minor=$(echo $version | cut -d. -f2)
   local patch=$(echo $version | cut -d. -f3)
-  patch=$((patch + 1))
+
+
+  case "$type" in
+    patch)
+      patch=$((patch + 1))
+      ;;
+    minor)
+      minor=$((minor + 1))
+      patch=0
+      ;;
+    *)
+      echo "Unknown version type: $type"
+      exit 1
+      ;;
+  esac
+
   echo "${major}.${minor}.${patch}"
 }
 
-new_version=$(increment_patch_version $current_version)
+new_version=$(increment_patch_version $current_version $version_type)
 
 jq --arg new_version "$new_version" '.version = $new_version' package.json > package.tmp.json && mv package.tmp.json package.json
 jq --arg new_version "$new_version" '.version = $new_version' src/manifest/base.json > src/manifest/base.tmp.json && mv src/manifest/base.tmp.json src/manifest/base.json
